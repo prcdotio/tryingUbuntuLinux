85a9826f97419ee03a9cbd0857a2b6c404e68e3b
prcdotio <chaudharyposhan57@gmail.com>
2026-01-25 23:02:13 +0545

    Uploading factoraL compiled file to calculate factoral of input number but with no strict validation

--- C-Programming[Practice&Recall]/main.c
+++ C-Programming[Practice&Recall]/main.c
@@ -1,6 +1,18 @@
+// Trying recursion to calculate factoral
+
 #include <stdio.h>
+int fact(int num);
 int main(void){
-    printf("Hello World!\n");
+    int n;
+    printf("Enter any number to get it's corresponding factoral:\t");
+    scanf("%d", &n);
+    int result = fact(n);
+    printf("The corresponding factoral is: %d\n", result);
 
     return 0;
+}
+int fact(int num){
+    if(num == 1 || num == 0)
+    return 1;
+    return num * fact(num - 1);
 }
