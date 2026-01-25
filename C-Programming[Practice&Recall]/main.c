fbba46433688554f946fa66e9f8d0d17e5f190fe
prcdotio <chaudharyposhan57@gmail.com>
2026-01-25 23:24:10 +0545

    also added warning for higher value [limit 12] otherwise output will be wired message. #update

--- C-Programming[Practice&Recall]/main.c
+++ C-Programming[Practice&Recall]/main.c
@@ -9,9 +9,13 @@ int main(void){
     if(n < 0){
         printf("F*ck U, Don't try to be oversmart. It's undefined for negative itegers.\n");
         return -1;
-    }
+    }else if(n > 12){
+        printf("F*c* U, Don't try to be oversmart. It's just 64 bit system and compiler is just built for 32 bit handling. Don't try to calculate larger number otherwise you will get inaccurate value.\n");
+        return 13;
+    }else{
     int result = fact(n);
     printf("The corresponding factoral is: %d\n", result);
+    }
 
     return 0;
 }
